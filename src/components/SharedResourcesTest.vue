aa76cadddb96163defe9a942aba369f89d016d2d
Mikko Tenni <mikko.tenni@sympa.com>
2021-04-01 10:46:23 +0300

    Add helpers

--- src/components/SharedResourcesTest.vue
+++ src/components/SharedResourcesTest.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
-    <p>I'm using Lodash in some {{ msg }} to see if it works.</p>
+    <p>I'm using Lodash in some {{ msg }} to see if it works. Is empty or whitespace is {{ isEmpty }}.</p>
   </div>
 </template>
 
 <script>
 import Vue from 'vue';
 import lodash from 'lodash';
+import Helpers from '../assets/shared/helpers.js';
 
 export default Vue.extend({
   name: 'SharedResourcesTest',
   data() {
     return {
       msg: lodash.concat(['useless'], ['way']).join(' '),
+      isEmpty: Helpers.isEmptyOrWhitespace('   ')
     };
   },
 });
